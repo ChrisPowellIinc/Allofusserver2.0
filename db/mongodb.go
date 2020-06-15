119ac18e34ad0025d54c4160aff1396ec0c5f767
Odohi David <odohi.david@gmail.com>
2020-06-15 03:28:19 +0100

    create mongo db implementation of the database interface

---
+++ db/mongodb.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"time"
+
+	"github.com/ChrisPowellIinc/Allofusserver2.0/models"
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+	"github.com/pkg/errors"
+)
+
+// MongoDB implements the DB interface
+type MongoDB struct {
+	DB *mgo.Database
+}
+
+// Init sets up the mongodb instance
+func (mdb *MongoDB) Init() {
+	DBSession, err := mgo.Dial("mongodb://localhost:27017/allofus")
+	if err != nil {
+		panic(errors.Wrap(err, "Unable to connect to Mongo database"))
+	}
+	mdb.DB = DBSession.DB("allofus")
+}
+
+// CreateUser creates a new user in the DB
+func (mdb MongoDB) CreateUser(user models.User) (models.User, error) {
+	user.CreatedAt = time.Now()
+	err := mdb.DB.C("user").Insert(&user)
+	return user, err
+}
+
+func (mdb MongoDB) FindUserByUsername(username string) (models.User, error) {
+	var user models.User
+	err := mdb.DB.C("user").Find(bson.M{"username": username}).One(&user)
+	return user, err
+}
