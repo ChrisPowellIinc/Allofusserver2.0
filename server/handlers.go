3c7d2194febe8fd714853c67f88413d44a7d4b69
Odohi David <odohi.david@gmail.com>
2020-06-15 03:29:06 +0100

    implement the signup and login handlers

--- server/handlers.go
+++ server/handlers.go
@@ -1,13 +1,49 @@
 package server
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/ChrisPowellIinc/Allofusserver2.0/models"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func (s *Server) handleSignup() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var user models.User
+
+		if err := c.ShouldBindJSON(&user); err != nil {
+			errs := []string{}
+			for _, fieldErr := range err.(validator.ValidationErrors) {
+				errs = append(errs, fieldError{fieldErr}.String())
+				// c.JSON(http.StatusBadRequest, fieldError{fieldErr}.String())
+				// return // exit on first error
+			}
+			c.JSON(http.StatusBadRequest, gin.H{"errors": errs})
+			return
+		}
+		// TODO: validation
+		var err error
+		user.Password, err = bcrypt.GenerateFromPassword([]byte(user.PasswordString), bcrypt.DefaultCost)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "Sorry a problem occured, please try again",
+				"status":  http.StatusInternalServerError,
+			})
+			return
+		}
+		user, err = s.DB.CreateUser(user)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "Sorry a problem occured, please try again",
+				"Status":  http.StatusInternalServerError,
+			})
+			return
+		}
+
 		c.JSON(http.StatusCreated, gin.H{
 			"message": "signup successful",
 		})
@@ -16,8 +52,69 @@ func (s *Server) handleSignup() gin.HandlerFunc {
 
 func (s *Server) handleLogin() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var user models.User
+		type Login struct {
+			Username string `json:"username"`
+			Password string `json:"password"`
+		}
+		var loginRequest Login
+		if err := c.ShouldBindJSON(&loginRequest); err != nil {
+			errs := []string{}
+			if err, ok := err.(validator.ValidationErrors); ok {
+				for _, fieldErr := range err {
+					errs = append(errs, fieldError{fieldErr}.String())
+				}
+				c.JSON(http.StatusBadRequest, gin.H{"errors": errs})
+			} else {
+				c.JSON(http.StatusUnauthorized, gin.H{"errors": []string{"username or password incorrect"}})
+			}
+			return
+		}
+		// Check if the user with that username exists
+		user, err := s.DB.FindUserByUsername(loginRequest.Username)
+		if err != nil {
+			log.Printf("No user: %v\n", err)
+			c.JSON(http.StatusUnauthorized, gin.H{"errors": []string{"username or password incorrect"}})
+			return
+		}
+		err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginRequest.Password))
+		if err != nil {
+			log.Printf("Passwords do not match %v\n", err)
+			c.JSON(http.StatusUnauthorized, gin.H{"errors": []string{"username or password incorrect"}})
+			return
+		}
+
+		// Create a new token object, specifying signing method and the claims
+		// you would like it to contain.
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{"user_email": user.Email})
+
+		// Sign and get the complete encoded token as a string using the secret
+		// TODO: use env variables to store secret
+		tokenString, err := token.SignedString([]byte("JWTSecret"))
+
+		// _, token, err := jwt.TokenAuth.Encode(jwtauth.Claims{"user_email": user.Email})
+
+		if err != nil {
+			log.Printf("token signing err %v\n", err)
+			c.JSON(http.StatusUnauthorized, gin.H{"errors": []string{"username or password incorrect"}})
+			return
+		}
+
+		// Data: map[string]interface{}{
+		// 	"token":      tokenString,
+		// 	"first_name": user.FirstName,
+		// 	"last_name":  user.LastName,
+		// 	"phone":      user.Phone,
+		// 	"email":      user.Email,
+		// 	"username":   user.Username,
+		// 	"image":      user.Image,
+		// },
 		c.JSON(http.StatusOK, gin.H{
 			"message": "login successful",
+			"data": map[string]interface{}{
+				"user":  user,
+				"token": tokenString,
+			},
 		})
 	}
 }
