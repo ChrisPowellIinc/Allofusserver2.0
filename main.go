de63567a3ab623479de934c2b59bc525476ae71c
Odohi David <odohi.david@gmail.com>
2020-06-15 03:30:19 +0100

    use mongo db for starting the applicatoin

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"github.com/spankie/aou/db"
-	"github.com/spankie/aou/router"
-	"github.com/spankie/aou/server"
+	"github.com/ChrisPowellIinc/Allofusserver2.0/db"
+	"github.com/ChrisPowellIinc/Allofusserver2.0/router"
+	"github.com/ChrisPowellIinc/Allofusserver2.0/server"
 )
 
 func main() {
+	DB := db.MongoDB{}
+	DB.Init()
 	s := &server.Server{
-		DB: db.DB{
-			Users: make(db.Users, 0),
-		},
+		DB:     DB,
 		Router: router.NewRouter(),
 	}
 	s.Start()
