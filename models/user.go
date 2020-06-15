83f14fd55bb4269500cc2b940ec2e6ec8952fbd2
Odohi David <odohi.david@gmail.com>
2020-06-15 03:28:45 +0100

    fill out the user model structs

--- models/user.go
+++ models/user.go
@@ -1,9 +1,23 @@
 package models
 
+import "time"
+
 // User holds a user details
 type User struct {
-	ID        uint64 `json:"id"`
-	Name      string `json:"name"`
-	Type      string `json:"type"`
-	SessionID int    `json:"session_id"`
+	FirstName      string    `json:"first_name" bson:"first_name,omitempty" binding:"required" form:"first_name"`
+	LastName       string    `json:"last_name" bson:"last_name,omitempty" binding:"required" form:"last_name"`
+	Phone          string    `json:"phone,omitempty" bson:"phone,omitempty" binding:"required" form:"phone"`
+	Email          string    `json:"email" bson:"email,omitempty" binding:"required,email" form:"email"`
+	Username       string    `json:"username" bson:"username,omitempty" binding:"required" form:"username"`
+	Password       []byte    `json:"-" bson:"password,omitempty"`
+	PasswordString string    `json:"password,omitempty" bson:"-" binding:"required" form:"password"`
+	Reset          string    `json:"-" bson:"reset"`
+	Image          string    `json:"image,omitempty" bson:"image,omitempty"`
+	CreatedAt      time.Time `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	AccessToken    string    `json:"token,omitempty" bson:"token,omitempty"`
+	// stripe payment details
+	SessionID       string `json:"session_id,omitempty" bson:"session_id,omitempty"`
+	SetupIntentID   string `json:"setup_intent_id,omitempty" bson:"setup_intent_id,omitempty"`
+	PaymentMethodID string `json:"payment_method_id,omitempty" bson:"payment_method_id,omitempty"`
+	CustomerID      string `json:"customer_id,omitempty" bson:"customer_id,omitempty"`
 }
