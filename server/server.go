c62834e2d61342fe2bfed6d7947cda0fda6a7b37
Odohi David <odohi.david@gmail.com>
2020-06-15 22:32:54 +0100

    add colon to port from env variable

--- server/server.go
+++ server/server.go
@@ -95,8 +95,8 @@ func (s *Server) setupRouter() *gin.Engine {
 // like router
 func (s *Server) Start() {
 	r := s.setupRouter()
-	PORT := os.Getenv("PORT")
-	if PORT == "" {
+	PORT := fmt.Sprintf(":%s", os.Getenv("PORT"))
+	if PORT == ":" {
 		PORT = ":8080"
 	}
 	srv := &http.Server{
