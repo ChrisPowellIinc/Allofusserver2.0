6bdb8169a068af854b8c320b9009590a975ac786
danvixent <danvixent@gmail.com>
2020-06-25 00:28:32 +0100

    add more methods to DB interface

--- db/db.go
+++ db/db.go
@@ -4,8 +4,9 @@ import "github.com/ChrisPowellIinc/Allofusserver2.0/models"
 
 // DB provides access to the different db
 type DB interface {
-	CreateUser(user models.User) (models.User, error)
-	FindUserByUsername(username string) (models.User, error)
-	FindUserByEmail(email string) (models.User, error)
-	PutInBlackList(blacklist models.Blacklist) error //TODO might remove this?
+	CreateUser(user *models.User) (*models.User, error)
+	FindUserByUsername(username string) (*models.User, error)
+	FindUserByEmail(email string) (*models.User, error)
+	FindUserByPhone(phone string) (models.User, error)
+	AddToBlackList(blacklist models.Blacklist) error //TODO might remove this?
 }
