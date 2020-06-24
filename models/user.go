b385561c615a79ec177357eef631d619e8664984
danvixent <danvixent@gmail.com>
2020-06-24 16:07:21 +0100

    add Status and CreatedAt fields to User model

--- models/user.go
+++ models/user.go
@@ -13,7 +13,9 @@ type User struct {
 	PasswordString string    `json:"password,omitempty" bson:"-" binding:"required" form:"password"`
 	Reset          string    `json:"-" bson:"reset"`
 	Image          string    `json:"image,omitempty" bson:"image,omitempty"`
+	Status         string    `json:"status,omitempty"` //TODO should this have the "-" tag?
 	CreatedAt      time.Time `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt      time.Time `json:"updated_at,omitempty"`
 	AccessToken    string    `json:"token,omitempty" bson:"token,omitempty"`
 	// stripe payment details
 	SessionID       string `json:"session_id,omitempty" bson:"session_id,omitempty"`
@@ -21,3 +23,31 @@ type User struct {
 	PaymentMethodID string `json:"payment_method_id,omitempty" bson:"payment_method_id,omitempty"`
 	CustomerID      string `json:"customer_id,omitempty" bson:"customer_id,omitempty"`
 }
+
+// FIRST
+// Add Status field to the user's model (Status string `json:"status"`)
+// Add updated_at field
+
+// 1.
+// create a blacklist collection (MongoDB)
+// add the token to blacklist
+// create middleware that would check that there is token in the request header (Authorization: Bearer token)
+// verify the token.
+// make sure you check the db for the email and make sure the status is active.
+
+// type blacklist struct {email string, token: string, created_at, time.Time}
+// Logout method: get the token and save it in the blacklist collection.
+
+// Profile page where user details would be shown and updated.
+// Timeline page: list all the users, and get each user details
+// User details page: show details of a user
+
+// 2.
+// Endpoint for returning loggedin user details
+// 3.
+// Update user detail (DO NOT UPDATE: username, email, password, status)
+// update the updated_at field of the user model to time.Now()
+// 4.
+// endpoint to list all users
+// 5.
+// get a user details based on the username passed to it.
