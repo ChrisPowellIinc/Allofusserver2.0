45e017baa3cfc9d6d65fdf83dbeb630ebdd6e23e
danvixent <danvixent@gmail.com>
2020-06-25 12:09:03 +0100

    add AuthorizeAndGetClaims

--- services/auth.go
+++ services/auth.go
@@ -7,12 +7,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// GetTokenFromHeader returns the token string in the authorization header
 func GetTokenFromHeader(c *gin.Context) string {
 	authHeader := c.Request.Header.Get("Authorization")
 	return authHeader[7:]
 }
 
 //TODO more research on verifying tokens
+
+// VerifyToken verifies a token
 func VerifyToken(tokenString string, claims jwt.MapClaims, secret string) (*jwt.Token, error) {
 	return jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
@@ -20,4 +23,14 @@ func VerifyToken(tokenString string, claims jwt.MapClaims, secret string) (*jwt.
 		}
 		return []byte(secret), nil
 	})
+}
+
+// AuthorizeAndGetClaims authorizes a context and returns the claims in its token
+func AuthorizeAndGetClaims(c *gin.Context, secret string) (jwt.MapClaims, error) {
+	tokenString := GetTokenFromHeader(c)
+	claims := jwt.MapClaims{}
+	if _, err := VerifyToken(tokenString, claims, secret); err != nil {
+		return nil, fmt.Errorf("error getting token: %v", err)
+	}
+	return claims, nil
 }
