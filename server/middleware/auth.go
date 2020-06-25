5405ba2c4b6f8056e609df8419166e7c083fa472
danvixent <danvixent@gmail.com>
2020-06-25 13:41:37 +0100

    call AuthorizeAndGetClaims to reduce code

--- server/middleware/auth.go
+++ server/middleware/auth.go
@@ -1,33 +1,25 @@
 package middleware
 
 import (
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/ChrisPowellIinc/Allofusserver2.0/services"
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
 // Authorize authorizes a request
 func Authorize(c *gin.Context) {
-	tokenString := services.GetTokenFromHeader(c)
-	if tokenString == "" {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
-		return
-	}
-
-	secret := os.Getenv("JWTSecret") //TODO must change to const
-	claims := jwt.MapClaims{}
-	token, err := services.VerifyToken(tokenString, claims, secret)
+	claims, err := services.AuthorizeAndGetClaims(c, os.Getenv("JWTSecret"))
 	if err != nil {
+		log.Printf("%v\n", err)
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
-		return
 	}
 
 	// set the claims as context parameter.
 	// so that the actions can use the claims from jwt token
-	c.Set("claims", token.Claims)
+	c.Set("claims", claims)
 	// calling next handler
 	c.Next()
 }
