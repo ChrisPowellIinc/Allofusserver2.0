fd881d68d0f20522b68f5c34f8d63d8e0a89fe30
danvixent <danvixent@gmail.com>
2020-06-26 11:15:45 +0100

    removed comments

--- models/user.go
+++ models/user.go
@@ -23,34 +23,3 @@ type User struct {
 	PaymentMethodID string `json:"payment_method_id,omitempty" bson:"payment_method_id,omitempty"`
 	CustomerID      string `json:"customer_id,omitempty" bson:"customer_id,omitempty"`
 }
-
-// FIRST
-// Add Status field to the user's model (Status string `json:"status"`)
-// Add updated_at field
-
-// 1.
-// create a blacklist collection (MongoDB)
-// add the token to blacklist
-// create middleware that would check that there is token in the request header (Authorization: Bearer token)
-// verify the token.
-// make sure you check the db for the email and make sure the status is active.
-//TODO but we dont check the status tho?
-
-// type blacklist struct {email string, token: string, created_at, time.Time}
-// Logout method: get the token and save it in the blacklist collection.
-
-//
-//
-// Profile page where user details would be shown and updated.
-// Timeline page: list all the users, and get each user details
-// User details page: show details of a user
-
-// 2.
-// Endpoint for returning loggedin user details
-// 3.
-// Update user detail (DO NOT UPDATE: username, email, password, status)
-// update the updated_at field of the user model to time.Now()
-// 4.
-// endpoint to list all users
-// 5.
-// get a user details based on the username passed to it.
