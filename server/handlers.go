a745608983d1ddc9fba93d5eb700ec288a6b8b3d
Odohi David <odohi.david@gmail.com>
2020-06-26 01:51:02 +0100

    update the existing methods and implement more handlers

--- server/handlers.go
+++ server/handlers.go
@@ -3,27 +3,30 @@ package server
 import (
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/ChrisPowellIinc/Allofusserver2.0/db"
 	"github.com/ChrisPowellIinc/Allofusserver2.0/models"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator"
+	validator "github.com/go-playground/validator/v10"
 	"golang.org/x/crypto/bcrypt"
 )
 
-// TODO: use env file to store secret
-const JWTSecret = "JWTSecret"
-
 func (s *Server) handleSignup() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var user *models.User
+		user := &models.User{}
 
 		if err := c.ShouldBindJSON(user); err != nil {
 			errs := []string{}
-			for _, fieldErr := range err.(validator.ValidationErrors) {
-				errs = append(errs, fieldError{fieldErr}.String())
+			verr, ok := err.(validator.ValidationErrors)
+			if ok {
+				for _, fieldErr := range verr {
+					errs = append(errs, fieldError{fieldErr}.String())
+				}
+			} else {
+				errs = append(errs, "internal server error")
 			}
 			c.JSON(http.StatusBadRequest, gin.H{"errors": errs})
 			return
@@ -64,7 +67,7 @@ func (s *Server) handleSignup() gin.HandlerFunc {
 
 func (s *Server) handleLogin() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var user *models.User
+		user := &models.User{}
 		type Login struct {
 			Username string `json:"username" binding:"required"`
 			Password string `json:"password" binding:"required"`
@@ -102,9 +105,7 @@ func (s *Server) handleLogin() gin.HandlerFunc {
 		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{"user_email": user.Email})
 
 		// Sign and get the complete encoded token as a string using the secret
-		tokenString, err := token.SignedString([]byte(JWTSecret))
-
-		// _, token, err := jwt.TokenAuth.Encode(jwtauth.Claims{"user_email": user.Email})
+		tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
 
 		if err != nil {
 			log.Printf("token signing err %v\n", err)
@@ -144,10 +145,13 @@ func (s *Server) handleLogout() gin.HandlerFunc {
 						blacklist.CreatedAt = time.Now()
 						blacklist.Token = token.Raw
 
-						if err := s.DB.AddToBlackList(blacklist); err != nil {
+						err := s.DB.AddToBlackList(blacklist)
+						if err != nil {
 							c.JSON(http.StatusInternalServerError, gin.H{"error": "logout failed"})
 							return
 						}
+						c.JSON(http.StatusOK, gin.H{"message": "logout sucessful"})
+						return
 					}
 				}
 			}
@@ -157,7 +161,9 @@ func (s *Server) handleLogout() gin.HandlerFunc {
 		return
 	}
 }
-func (s *Server) showProfile() gin.HandlerFunc {
+
+// handleShowProfile returns user's details
+func (s *Server) handleShowProfile() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		if userI, exists := c.Get("user"); exists {
 			if user, ok := userI.(*models.User); ok {
@@ -177,29 +183,140 @@ func (s *Server) showProfile() gin.HandlerFunc {
 	}
 }
 
-func (s *Server) updateUserDetails() gin.HandlerFunc {
+func (s *Server) handleUpdateUserDetails() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var updteUser *models.User
-		if err := c.ShouldBindJSON(updteUser); err != nil {
-			errs := []string{}
-			for _, fieldErr := range err.(validator.ValidationErrors) {
-				errs = append(errs, fieldError{fieldErr}.String())
-			}
-			c.JSON(http.StatusBadRequest, gin.H{"errors": errs})
-			return
-		}
-
 		if userI, exists := c.Get("user"); exists {
 			if user, ok := userI.(*models.User); ok {
-
-				if err := s.DB.UpdateUser(user, updteUser); err != nil {
+				username, email := user.Username, user.Email
+				if err := c.ShouldBindJSON(user); err != nil {
+					errs := []string{}
+					for _, fieldErr := range err.(validator.ValidationErrors) {
+						errs = append(errs, fieldError{fieldErr}.String())
+					}
+					c.JSON(http.StatusBadRequest, gin.H{"errors": errs})
+					return
+				}
+				user.Username, user.Email = username, email
+				if err := s.DB.UpdateUser(user); err != nil {
+					log.Printf("update user error : %v\n", err)
 					c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
 					return
 				}
+				c.JSON(http.StatusOK, gin.H{"message": "user updated successfuly"})
 				return
 			}
 		}
+
 		log.Printf("can't get user from context\n")
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
 	}
 }
+
+func (s *Server) handleGetUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if userI, exists := c.Get("user"); exists {
+			if user, ok := userI.(*models.User); ok {
+				users, err := s.DB.FindAllUsersExcept(user.Email)
+				if err != nil {
+					log.Printf("find users error : %v\n", err)
+					c.JSON(http.StatusInternalServerError, gin.H{"error": "could not find users"})
+					return
+				}
+				c.JSON(http.StatusOK, gin.H{"data": users, "message": "retrieved users sucessfully"})
+				return
+			}
+		}
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+}
+
+/*
+func uploadFileToS3(file multipart.File, fileName string, size int64, con *config.Config) error {
+	// get the file size and read
+	// the file content into a buffer
+	buffer := make([]byte, size)
+	file.Read(buffer)
+
+	// config settings: this is where you choose the bucket,
+	// filename, content-type and storage class of the file
+	// you're uploading
+	_, err := s3.New(con.AwsSession).PutObject(&s3.PutObjectInput{
+		Bucket:               aws.String(con.Constants.S3Bucket),
+		Key:                  aws.String(fileName),
+		ACL:                  aws.String("public-read"),
+		Body:                 bytes.NewReader(buffer),
+		ContentLength:        aws.Int64(int64(size)),
+		ContentType:          aws.String(http.DetectContentType(buffer)),
+		ContentDisposition:   aws.String("attachment"),
+		ServerSideEncryption: aws.String("AES256"),
+		StorageClass:         aws.String("INTELLIGENT_TIERING"),
+	})
+	return err
+}
+
+// UploadProfilePic uploads a user's profile picture
+func (handler *Handler) UploadProfilePic(w http.ResponseWriter, r *http.Request) {
+	userEmail, err := jwt.GetLoggedInUserEmail(r.Context())
+	if err != nil {
+		log.Println(err)
+		models.HandleResponse(w, r, "Unable to retrieve authenticated user.", http.StatusUnauthorized, nil)
+		return
+	}
+
+	maxSize := int64(2048000) // allow only 2MB of file size
+
+	err = r.ParseMultipartForm(maxSize)
+	if err != nil {
+		log.Println(err)
+		models.HandleResponse(w, r, "Image too large", http.StatusBadRequest, nil)
+		return
+	}
+
+	file, fileHeader, err := r.FormFile("profile_picture")
+	if err != nil {
+		log.Println(err)
+		models.HandleResponse(w, r, "Image not supplied", http.StatusBadRequest, nil)
+		return
+	}
+	defer file.Close()
+
+	// TODO:: Check for file type...
+	supportedFileTypes := map[string]bool{
+		".png":  true,
+		".jpeg": true,
+		".jpg":  true,
+	}
+	filetype := filepath.Ext(fileHeader.Filename)
+	if !supportedFileTypes[filetype] {
+		log.Println(filetype)
+		models.HandleResponse(w, r, "This image file type is not supported", http.StatusBadRequest, nil)
+		return
+	}
+	tempFileName := "profile_pics/" + bson.NewObjectId().Hex() + filetype
+	err = uploadFileToS3(file, tempFileName, fileHeader.Size, handler.config)
+	if err != nil {
+		log.Println(err)
+		models.HandleResponse(w, r, "An Error occured while uploading the image", http.StatusInternalServerError, nil)
+		return
+	}
+
+	imageURL := "https://s3.us-east-2.amazonaws.com/www.all-of.us/" + tempFileName
+
+	err = handler.config.DB.C("user").Update(bson.M{"email": userEmail}, bson.M{"$set": bson.M{"image": imageURL}})
+	if err != nil {
+		log.Println(err)
+		models.HandleResponse(w, r, "Unable to update user's email.", http.StatusInternalServerError, nil)
+		return
+	}
+
+	res := models.Response{}
+	res.Message = "Successfully Created File"
+	res.Status = http.StatusOK
+	res.Data = map[string]interface{}{
+		"imageurl": imageURL,
+	}
+	render.Status(r, http.StatusOK)
+	render.JSON(w, r, res)
+}
+*/
