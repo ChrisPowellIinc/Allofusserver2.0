14e8b22c12b1897a86ab3c526d090f49937df150
danvixent <danvixent@gmail.com>
2020-06-27 17:56:02 +0100

    add respond and decode helper methods

--- server/response.go
+++ server/response.go
@@ -1,10 +1,11 @@
 package server
 
 import (
-	"encoding/json"
 	"net/http"
 
+	"github.com/ChrisPowellIinc/Allofusserver2.0/servererrors"
 	"github.com/gin-gonic/gin"
+	validator "github.com/go-playground/validator/v10"
 )
 
 func (s *Server) respond(c *gin.Context, message string, status int, data interface{}, errs []string) {
@@ -17,6 +18,19 @@ func (s *Server) respond(c *gin.Context, message string, status int, data interf
 	c.JSON(status, responsedata)
 }
 
-func (s *Server) decode(w http.ResponseWriter, r *http.Request, v interface{}) error {
-	return json.NewDecoder(r.Body).Decode(v)
+// decode decodes the body of c into v
+func (s *Server) decode(c *gin.Context, v interface{}) []string {
+	if err := c.ShouldBindJSON(v); err != nil {
+		errs := []string{}
+		verr, ok := err.(validator.ValidationErrors)
+		if ok {
+			for _, fieldErr := range verr {
+				errs = append(errs, servererrors.NewFieldError(fieldErr).String())
+			}
+		} else {
+			errs = append(errs, "internal server error")
+		}
+		return errs
+	}
+	return nil
 }
